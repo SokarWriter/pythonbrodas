44c10aa748606848bf0933bf26f47efae4b15d6e
SokarWriter <oscar.saez.n@gmail.com>
2021-06-01 00:34:00 +0200

    masterclass dart jugones

--- 01 Adivino/adivino2.py
+++ 01 Adivino/adivino2.py
@@ -1,14 +1,37 @@
 from random import randint 
 #recoger trys del parametro
-trys = 5
+trys = 20
+num_trys = 0
 #recoger num_max del parametro
 num_max = 100
 #generar numero aleatorio entre 0 - num_max
 result = randint(0, num_max)
+
+print(result)
 #menu de incio
+print ("¿¿Hola jugador, tu mision es adivinar el numero en el que piensa la maquina...podras hacerlo??")
+saludo = input ()
+print ("recuerda que debes pensar bien tu pregunta.. solo dispones de " + str(trys) + " intentos para conseguirlo")
+advertencia = input ()
+print ("")
 #entramos partida
-#pedir al usuario su num
-#por cada input del jugon 1 try -
+while True: 
+    #pedir al usuario su num
+    num = int (input())
+    #por cada input del jugon 1 try - (num_trys: num_trys += 1)
+    num_trys = num_trys + 1
+    #comprobar el input del jugon si es el correcto
+    if num == result:
+        print("you win")
+        break
+    if num > num_max:
+        print("Te has pasado pero si sgues intentandolo el resultado es inferior a " + str (num_max) )
+        
+    elif num_trys == 1: 
+        print("Templado")
+    # ha surgido un problema, tmb tiene que comprobar si se ha pasado 
+    
+
 #comprobar el input del jugon si es el correcto
     #si el num es correcto "gana" 
     #si no acierta en el primer try la pista es "templado"
